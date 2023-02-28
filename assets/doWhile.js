4d57fe2548597c1feff9b0ebc31c1d46c2f08183
Kamilla <113370998+Kanin60@users.noreply.github.com>
2023-02-28 09:40:46 +0100

    opgave

---
+++ assets/doWhile.js
@@ -0,0 +1,27 @@
+/*Opgave 1
+Du skal to let-variabler. Den første variable skal hedde text og sættes til en tom tekststreng
+Den anden variable skal hedde i og sættes til 0. 
+Skriv variablerne her: */
+
+
+/* Her skal du skrive en while statement med betingelserne i < 10. 
+Du skal sætte variablen text ind i statementen og tillægge en tekststreng med teksten "<br>The number is" og plus i.
+Derefter skal i inctrementes. */
+
+/* Her skal skal variablen text sættes på HTML-elementet med id'et "while" ved hjælpe af innerHTML*/
+
+
+
+
+
+/*Opgave 2 
+Du skal to let-variabler. Den første variable skal hedde besked og sættes til en tom tekststreng
+Den anden variable skal hedde K og sættes til 0. 
+Skriv variablerne her:*/
+
+/* Her skal du skrive en do statement. 
+Du skal sætte variablen besked ind i statementen og tillægge K plus en tekststreng med teksten "<br>".
+Derefter skal K inctrementes. */
+
+/* Her skal du skrive en while statement med betingelserne K < 5. 
+ Inde i while statementen skal variablen besked sættes på HTML-elementet med id'et "doWhile" ved hjælpe af innerHTML*/
